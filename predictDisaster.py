03778d6600a242b01ee79839134dd4981ca9a346
MoumitaKamal <mkamal42@students.tntech.edu>
2020-03-03 03:32:48 -0600

    simple NLP

--- predictDisaster.py
+++ predictDisaster.py
@@ -6,5 +6,20 @@ Created on Mon Mar  2 13:28:38 2020
 """
 
 import pandas as pd
-dataset = pd.read_csv('train.csv')
+train_dataset = pd.read_csv('Datasets/train.csv')
+test_dataset = pd.read_csv('Datasets/test.csv')
 
+from sklearn import feature_extraction, linear_model, model_selection, preprocessing
+count_vectorizer = feature_extraction.text.CountVectorizer()
+
+X_train = count_vectorizer.fit_transform(train_dataset['text'])
+X_test = count_vectorizer.transform(test_dataset['text'])
+
+classifier = linear_model.RidgeClassifier()
+classifier.fit(X_train, train_dataset['target'])
+
+pred = classifier.predict(X_test)
+
+sub = pd.read_csv('sample_submission.csv')
+sub['target'] = classifier.predict(X_test)
+sub.to_csv('submission.csv', index = False)
